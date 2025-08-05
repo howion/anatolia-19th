29c9389a80df8cea4a67b720ee93f61843b690c9
howion <me@howion.com>
2025-08-05 17:58:59 +0300

    .

--- constants/app.ts
+++ constants/app.ts
@@ -3,17 +3,17 @@ const isProduction = process.env.NODE_ENV === 'production'
 export const App = {
     name: 'Anatolia: 19th Century',
     site: 'https://anatolia19.metu.edu.tr',
-    baseUrl: isProduction ? 'https://anatolia19th.vercel.app' : 'http://localhost:3000',
+    baseUrl: isProduction ? 'https://anatolia19.metu.edu.tr' : 'http://localhost:3000',
     isProduction,
-    version: '0.0.1',
+    version: '1.0.0',
     defaults: {
         description:
             'An undergraduate digital humanities project aiming to compile as well as digitize intricate data on the Anatolia in 19th century.',
         author: 'howion',
-        keywords: ['digital', 'humanities'],
+        keywords: ['anatolia', 'turkey', 'digital', 'humanities'],
         themeColor: '#0A3025'
     },
     api: {
-        baseUrl: isProduction ? 'https://anatolia19th.vercel.app/api' : 'http://localhost:3000/api'
+        baseUrl: isProduction ? 'https://anatolia19.metu.edu.tr/api' : 'http://localhost:3000/api'
     }
 } as const
